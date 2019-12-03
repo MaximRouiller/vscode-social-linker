94c117b254e625acd3f488013b6911c5f90be6db
Maxime Rouiller <angelzerosoft@gmail.com>
2019-12-03 13:30:14 -0500

    settings for alias

--- src/extension.ts
+++ src/extension.ts
@@ -13,14 +13,32 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('extension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World!');
-	});
+	}));
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('socialLinker.setAlias', async () =>{
+		let aliasConfiguration = vscode.workspace.getConfiguration("socialLinker").get('alias', '');
+		let alias = await vscode.window.showInputBox({
+			prompt: `'Please type in your alias. Leave empty to unset. (Current: ${aliasConfiguration})'`
+		});
+
+		// if alias is undefined, it means the user ESC out of the input. Make no changes.
+		if(alias === undefined) { return; }
+		
+		await vscode.workspace.getConfiguration('socialLinker').update('alias', alias === '' ? undefined : alias, true);
+		if(alias || alias !== '') {			
+			vscode.window.showInformationMessage(`'User alias set to ''${alias}'' '`);
+		}
+		else {
+			vscode.window.showWarningMessage('Alias has been unset.');
+		}
+	}));
+
+
 }
 
 // this method is called when your extension is deactivated
